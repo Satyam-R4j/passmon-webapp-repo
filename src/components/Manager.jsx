4d64fa8e8c2aa78f786025ffc4056851fe2df08f
Satyam-R4j <satyamraj418@gmail.com>
2025-01-20 01:02:47 +0530

    Some progress

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 
+
 const Manager = () => {
   return (
     <>
-      <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-        <div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
-      </div>
-
-      <div className=" bg-red-500 mycontainer">
-        <h1>PassMon</h1>
-        <p>Your own Password Manager</p>
-        <div className="text-white flex flex-col p-4">
-          <input className="rounded-full " type="text" />
-          <div className="flex">
-            <input type="text" />
-            <input type="text" />
+      <div class="absolute inset-0 -z-10 h-full w-full bg-green-100 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+      <div className="  mycontainer">
+        <h1 className="text-4xl text   text-center">
+          <span className="text-green-500">&lt;</span>
+          <span>Pass</span>
+          <span className="text-green-500">Mon/&gt;</span>
+        </h1>
+        <p className="text-green-500 text-xl text-center">
+          Your own Password Manager
+        </p>
+        <div className="text-black gap-8  flex flex-col items-center  p-4 ">
+          <input
+            className="rounded-xl border border-green-500 w-full p-4 py-1 "
+            type="text"
+            placeholder="Enter website URL"
+          />
+          <div className="flex w-full justify-between  gap-10 ">
+            <input
+              className="rounded-xl border border-green-500 w-full p-4 py-1 "
+              type="text"
+              placeholder="Enter the username"
+            />
+            <div className="relative">
+              <input
+                className="rounded-xl border border-green-500 w-full p-4 py-1 "
+                type="text"
+                placeholder="Enter the password"
+              />
+              <span className="absolute  right-0"><img className="p-1 w-[5vh]" src="src/assets/eye.png" alt="" /></span>
+            </div>
           </div>
+          <button className=" flex items-center justify-center text-xl bg-green-400 hover:bg-green-500 rounded-xl px-4 py-2 w-fit ">
+            <img src="src/assets/add-card.gif" alt="" className="w-8 h-8 " />
+            Add Password
+          </button>
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-green-400 ">
+    <nav className="bg-zinc-800 text-white">
       <div className=" mycontainer flex justify-between items-center px-4 py-5 h-14">
-        <div className="logo font-bold ">PassMon</div>
+        <div className="logo font-bold text-white text-2xl">
+          <span className="text-green-500">&lt;</span>
+          <span>Pass</span>
+          <span className="text-green-500">Mon/&gt;</span>
+        </div>
         <ul>
           <li className="flex gap-5">
             <a className="hover:font-bold" href="/">
